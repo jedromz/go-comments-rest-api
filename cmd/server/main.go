0616a39e7eb4569e6bd7088076d26b883b35ce83
Jędrzej Romankiewicz <romankiewicz.j@gmail.com>
2023-02-22 15:56:47 +0100

    db

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	database "go-comments-rest-api/db"
+)
 
 // Run - responsible for the instantiation
 // and startup of our application
 func Run() error {
 	fmt.Println("starting our application")
+	db, err := database.NewDatabase()
+	if err != nil {
+		fmt.Println("failed to connect to the database")
+		return err
+	}
+	if err := db.Ping(context.Background()); err != nil {
+		return err
+	}
 	return nil
 }
 
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module go-comments-rest-api
 
 go 1.19
+
+require (
+	github.com/jmoiron/sqlx v1.3.5 // indirect
+	github.com/lib/pq v1.10.7 // indirect
+	github.com/sirupsen/logrus v1.9.0 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+)
